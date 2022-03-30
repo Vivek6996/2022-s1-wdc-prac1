72a8d1df6ad0755c0e57ca51961488ea6ead1265
Vivek6996 <73323788+Vivek6996@users.noreply.github.com>
2022-03-30 10:31:32 +0000

    task3.2

--- prac3/task3.js
+++ prac3/task3.js
@@ -1,3 +1,4 @@
+
 var count = 1;
 
 function counter(){
@@ -10,27 +11,21 @@ function counter(){
 
 }
 
-var content = document.getElementsByClassName("post-content");
+var postC = "";
 
-    var dt = new Date();
-    var dt2 = document.getElementsByClassName("post-time");
-
-    show = show + dt + "<br/>"
-    + content[0].value + "<br/>";
-    dt2[0].innerHTML = show;
-
-
-
-
-    const t = new Date();
-    document.getElementById("posts").innerHTML = t;
-    var postC = document.getElementsByClassName("posts").innerText = display;
-
-
+function getDatenTime(){
 
     var t = new Date();
     var dTime = document.getElementsByClassName("post-time");
     dTime[0].innerHTML = t;
 
-    var postC = document.getElementsByClassName("post-content");
+    postC = document.getElementsByClassName("post-content");
     postC[0].innerHTML = document.getElementsByTagName("TEXTAREA")[0].value;
+
+}
+
+
+
+
+
+
